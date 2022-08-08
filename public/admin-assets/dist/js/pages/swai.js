4853ea78b212420a76b221d6d5e13ea96fe22cc2
Swai-D <davyswai@gmail.com>
2022-08-08 07:53:52 +0300

    final-set-up

---
+++ public/admin-assets/dist/js/pages/swai.js
@@ -0,0 +1,8 @@
+function printDiv(printMe) {
+  var printContents = document.getElementById(printMe).innerHTML;
+  var originalContents = document.body.innerHTML;
+
+  document.body.innerHTML = printContents;
+  window.print();
+  document.body.innerHTML = originalContents;
+}
--- resources/views/ReportBlade/view-report.blade.php
+++ resources/views/ReportBlade/view-report.blade.php
@@ -8,6 +8,7 @@
 <link rel="stylesheet" href="/admin-assets/plugins/fontawesome-free/css/all.min.css">
 <!-- Theme style -->
 <link rel="stylesheet" href="/admin-assets/dist/css/adminlte.min.css">
+<link rel="stylesheet" href="/admin-assets/my-css/print.css">
 @endsection
 
 @section('my-report-nav-active', 'active')
@@ -50,30 +51,14 @@
             </div>
           </div>
           <!-- /.card-header -->
-          <div class="card-body p-0">
+          <div class="card-body p-0" id="printMe">
             <div class="mailbox-read-info">
               <h5>{{$report->ReportSubject}}</h5>
               <h6>From: {{$report->userEmail}}
                 <span class="mailbox-read-time float-right">{{$report->created_at->format('d M Y, h:m')}}</span></h6>
             </div>
             <!-- /.mailbox-read-info -->
-            <div class="mailbox-controls with-border text-center">
-              <div class="btn-group">
-                <a href="" class="btn btn-danger btn-sm" data-container="body" title="Delete">
-                  <i class="far fa-trash-alt"></i>
-                </a>
-                <!-- <a href="/Multiflower-Report-System/reply-report"  class="btn btn-default btn-sm" data-container="body" title="Reply" style="color:gray;">
-                  <i class="fas fa-reply"></i>
-                </a> -->
-                <!-- <a  href="" class="btn btn-primary btn-sm" data-container="body" title="Forward">
-                  <i class="fas fa-share"></i>
-                </a> -->
-              </div>
-              <!-- /.btn-group -->
-              <button type="button" class="btn btn-success btn-sm" title="Print">
-                <i class="fas fa-print"></i>
-              </button>
-            </div>
+
             <!-- /.mailbox-controls -->
             <div class="mailbox-read-message">
               {!! $report->ReportBody !!}
@@ -88,8 +73,8 @@
               <!-- <a href="/Multiflower-Report-System/direct-message-show-page" class="btn btn-default"><i class="fas fa-reply" ></i> Reply</a> -->
               <!-- <a href="/Multiflower-Report-System/direct-message-home-page" class="btn btn-default"><i class="fas fa-share" style="color:blue;"></i> Forward</a> -->
             </div>
-            <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i> Delete</button>
-            <button type="button" class="btn btn-success"><i class="fas fa-print"></i> Print</button>
+            <br>
+            <button type="button" onclick="printDiv('printMe')" class="btn btn-success btn-lg float-right print-window"><i class="fas fa-print"></i> Print</button>
           </div>
           <!-- /.card-footer -->
         </div>
@@ -112,6 +97,7 @@
 <script src="/admin-assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="/admin-assets/dist/js/adminlte.min.js"></script>
+<script src="/admin-assets/dist/js/pages/swai.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="/admin-assets/dist/js/demo.js"></script>
 @endsection
